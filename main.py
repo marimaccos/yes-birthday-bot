73ce2d96236affb217232d9c4d89750808e62562
marimaccos <mariana_maccos@hotmail.com>
2022-10-28 01:24:25 -0300

    Updating start command and adding help command

--- main.py
+++ main.py
@@ -8,27 +8,42 @@ TOKEN = keys.token
 
 # Setting up logging module, so you will know when 
 # and why things don't work as expected
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 # Commands
-def start_command(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+def start(update: Update, context: CallbackContext):
+    """Send a message when the command /start is issued."""
+    context.bot.send_message(chat_id=update.effective_chat.id, text="yes!")
+
+def help_command(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                            text="Commands:"
+                                 "\n/start -- Starts the bot"
+                                 "\n/mybday [dd.mm.yyyy] -- Saves the user birthday"
+                                 "\n/next -- Shows the next birthday"
+                                 "\n/dates -- Shows the birthdays list"                                
+                                 "\n/help -- Shows the commands")
 
 
 def main():
-
+    """Start the bot."""
     # The Updater continuously fetches new updates from telegram and passes them on to the Dispatcher
     # Create Updater and pass it your bot's token.
     updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
 
-    start_handler = CommandHandler('start', start_command)
+    # On different commands - answer in Telegram
+    start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    # continuously make a request to the telegram servers and get new updates from there 
+    help_handler = CommandHandler('help', help_command)
+    dispatcher.add_handler(help_handler)
+
+    # Continuously make a request to the telegram servers and get new updates from there 
     # if they exists.
     updater.start_polling()
 
